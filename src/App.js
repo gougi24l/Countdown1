5b27b1db261dc96c5f040c7c936c5f2c2eba47aa
gougi24l <gougi24l@mtholyoke.edu>
2026-03-29 18:58:50 -0400

    add Game function

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function Square({value, onSquareClick}) {
   );
 }
 
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -55,6 +55,26 @@ export default function Board() {
     </>
   );
 }
+
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+  
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
+
+
 
 function calculateWinner(squares) {
   const lines = [
